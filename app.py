5fa9483a2e0cf6d01b3984e5a75c964a7d084c53
brunaagm <135059772+brunaagm@users.noreply.github.com>
2024-12-14 21:00:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -16,6 +16,7 @@ db = SQLAlchemy(app)
 # Inicializa o Flask-Migrate
 migrate = Migrate(app, db)
 
+
 # Definindo o modelo de dados para a tabela 'Musica'
 class Musica(db.Model):
     __tablename__ = 'musicas'
@@ -31,15 +32,36 @@ def create_tables():
     with app.app_context():
         db.create_all()  # Criação das tabelas
 
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    senha = db.Column(db.String(200), nullable=False)
+
+
 # Página de login
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        usuario = request.form.get('usuario')
+        senha = request.form.get('senha')
+         # Lógica de autenticação simples (por exemplo, verificação de usuário e senha)
+        if usuario == 'ex@gmail.com' and senha == 'senha123':
+            session['logged_in'] = True  # Marca o usuário como logado
+            return redirect(url_for('registrar'))  # Redireciona para a página de registrar músicas
+        else:
+            flash('Credenciais inválidas', 'error')
+
     return render_template('login.html')
 
 # Página de registro de músicas e listagem das músicas
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
-    if request.method == 'POST':
+     if 'logged_in' in session:
+        return redirect(url_for('login'))
+     if request.method == 'POST':
         musica_name = request.form.get('song_name')  # Correspondência com o nome do campo no HTML
         cantor = request.form.get('cantor_name')
 
@@ -58,8 +80,8 @@ def registrar():
         else:
             flash('Por favor, insira os dados.', 'error')
 
-    songs = Musica.query.all()  # Recupera todas as músicas
-    return render_template('registrar.html', songs=songs)
+     songs = Musica.query.all()  # Recupera todas as músicas
+     return render_template('registrar.html', songs=songs)
 
 # Rota para excluir uma música
 @app.route('/excluir/<int:id>')
@@ -74,3 +96,8 @@ def excluir_musica(id):
 if __name__ == '__main__':
     create_tables()  # Garantir que as tabelas sejam criadas no início
     app.run(debug=True)
+
+@app.route('\set_cookie')
+def set_cookie():
+    resp = make_response("Cookie has been set!")
+    resp.set_cookie('username', 'Atena', max_age=60*60*24)
